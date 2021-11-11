08a7ee6b352ef0628effeb44d1d075aa4dc09403
aliakbaralfaqih <ali.akbar@investree.id>
2021-11-11 20:51:43 +0700

    Fixing Task 1 ( Pagination, remove result, first letter name capital, add missing information )

--- src/main/kotlin/com/miniproject/pokedex/PokedexApplication.kt
+++ src/main/kotlin/com/miniproject/pokedex/PokedexApplication.kt
@@ -1,11 +1,16 @@
 package com.miniproject.pokedex
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAsync
 class PokedexApplication
 
 fun main(args: Array<String>) {
 	runApplication<PokedexApplication>(*args)
 }
+
--- src/main/kotlin/com/miniproject/pokedex/model/PokemonDetailData.kt
+++ src/main/kotlin/com/miniproject/pokedex/model/PokemonDetailData.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties("base_experience", "forms",
     "game_indices", "held_items", "is_default", "location_area_encounters", "moves",
-    "order", "past_types", "species", "sprites", "stats", "types")
+    "order", "past_types", "species", "stats")
 data class PokemonDetailData(
     @JsonProperty(value="id")
     var id: Int? = 0,
@@ -17,19 +17,32 @@ data class PokemonDetailData(
     var _weight: Int? = 0,
     @JsonProperty(value="abilities")
     var abilities: List<Abilities>? = null,
+    @JsonProperty(value="types")
+    var types: List<Types>? = null,
+    @JsonProperty(value="sprites")
+    var sprites: Sprites? = null
 )
 
 @JsonIgnoreProperties("slot")
 data class Abilities(
     @JsonProperty(value="ability")
-    var ability: Ability? = null,
+    var ability: PokemonData? = null,
 
     @JsonProperty(value="is_hidden")
     var isHidden: Boolean? = null
 )
 
-@JsonIgnoreProperties("url")
-data class Ability(
-    @JsonProperty(value="name")
-    var name: String? = null
+@JsonIgnoreProperties("slot")
+data class Types(
+    @JsonProperty(value="type")
+    var type: PokemonData? = null
+)
+
+@JsonIgnoreProperties("back_female", "back_shiny", "back_shiny_female",
+    "front_female", "front_shiny", "front_shiny_female", "other", "versions")
+data class Sprites(
+    @JsonProperty(value="back_default")
+    var backDefault: String? = null,
+    @JsonProperty(value="front_default")
+    var frontDefault: String? = null
 )
